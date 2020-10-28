9dbbd02c121bf90492769dcad669ea3159105062
dargaCode <darga.code@gmail.com>
2020-10-28 07:40:02 -0700

    Prototyping dynamic grid * Render tasks in the correct lane * Render some test dates along the top

--- src/components/timeline-grid/TimelineGrid.tsx
+++ src/components/timeline-grid/TimelineGrid.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { DATE_FORMAT } from "../../utils/dateConstants";
+import moment from "moment";
 import { Task } from "../../classes/task-scheduler/taskSchedulerUtils";
+import {
+  DATE_FORMAT,
+  DATE_FORMAT_TIMELINE_DATE
+} from "../../utils/dateConstants";
+import { getNSequentialDays } from "../../utils/dateUtils";
 import styles from "./TimelineGrid.module.scss";
 
 interface Props {
   tasks: Task[];
+  laneCount: number;
 }
 
 interface State {}
@@ -14,24 +20,42 @@ export default class TimelineGrid extends React.Component<Props, State> {
     super(props);
   }
 
-  render() {
-    const { tasks } = this.props;
+  render(): JSX.Element {
+    const { tasks, laneCount } = this.props;
+
+    // todo move this up one level? Add logic into TaskScheduler to track earliest and latest dates
+    // then these won't be hardcoded
+    const startDate = moment("2018-01-01", DATE_FORMAT);
+
+    const columnDates = getNSequentialDays(startDate, 10);
 
     return (
       <div className={styles.container}>
-        <div className={styles.grid}>
+        <div className={styles.dateRow}>
+          {/* todo extract to own method */}
+          {columnDates.map(date => {
+            return (
+              <div key={date.dayOfYear()} className={styles.date}>
+                {date.format(DATE_FORMAT_TIMELINE_DATE)}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className={styles.grid} style={{ background: "gray" }}>
+          {/* todo extract to Task component */}
+          {/* todo convert task start/end dates to grid columns */}
           {tasks.map(task => {
-            const { name, startDate, endDate, laneIndex, id } = task;
+            const { name, laneIndex, id } = task;
+            const rowNum = laneIndex + 1;
 
             return (
-              <div key={id}>
-                <h2>{name}</h2>
-
-                <ul>
-                  <li key={1}>Start: {startDate.format(DATE_FORMAT)}</li>
-                  <li key={2}>End: {endDate.format(DATE_FORMAT)}</li>
-                  <li key={3}>Lanes: {laneIndex}</li>
-                </ul>
+              <div
+                key={id}
+                className={styles.task}
+                style={{ gridRow: `${rowNum}` }}
+              >
+                {id} {name}
               </div>
             );
           })}
