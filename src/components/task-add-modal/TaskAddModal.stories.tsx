418d6d2257e28a955f38529ece3b7f8145d1e5eb
dargaCode <darga.code@gmail.com>
2020-11-01 17:41:04 -0800

    Add file stubs required for adding tasks

---
+++ src/components/task-add-modal/TaskAddModal.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TaskAddModal from "./TaskAddModal";
+
+export default {
+  title: "TaskAddModal",
+  component: TaskAddModal
+};
+
+export const Basic = (): JSX.Element => <TaskAddModal />;
---
+++ src/components/task-details-form/TaskDetailsForm.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TaskDetailsForm from "./TaskDetailsForm";
+
+export default {
+  title: "TaskDetailsForm",
+  component: TaskDetailsForm
+};
+
+export const Basic = (): JSX.Element => <TaskDetailsForm />;
