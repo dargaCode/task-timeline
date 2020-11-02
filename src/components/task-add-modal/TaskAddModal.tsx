babd9e0dd5669d3756530055fdcb3cf603557348
dargaCode <darga.code@gmail.com>
2020-11-01 18:57:47 -0800

    Add stub component

---
+++ src/components/task-add-modal/TaskAddModal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {}
+
+interface State {}
+
+export default class TaskAddModal extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render(): JSX.Element {
+    return <div>modal!</div>;
+  }
+}
